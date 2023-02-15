757485bd8585144d1b763e8814f77449ca24d273
vlad_nolimits <start2top@gmail.com>
2023-02-15 15:40:45 +0500

    initialized test-tast for Friend-Lee made html structure and main styles without range input  media and scripts

---
+++ gulpfile.js
@@ -0,0 +1,49 @@
+gulp = require('gulp'); 
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
+const rename = require("gulp-rename");
+const htmlmin = require('gulp-htmlmin');
+
+gulp.task('server', function() {
+
+    browserSync({
+        server: {
+            baseDir: "dist"
+        }
+    });
+
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('styles', function() {
+    return gulp.src("src/assets/sass/**/*.+(scss|sass)")
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(rename({suffix: '.min', prefix: ''}))
+
+        .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(gulp.dest("dist/css/"))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    gulp.watch("src/assets/sass/*.+(scss|sass|css)", gulp.parallel('styles'));
+    gulp.watch("src/*.html").on('change', gulp.parallel('html'));
+    gulp.watch("src/js/**/*.js").on('change', gulp.parallel('scripts'));
+});
+
+gulp.task('html', function () {
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src("src/js/*.js")
+        .pipe(gulp.dest("dist/js"))
+        .pipe(browserSync.stream());
+});
+
+
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'html'));
