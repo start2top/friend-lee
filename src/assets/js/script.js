99885315b2ab6be7024472be4ff3eb9b3206e1e5
vlad_nolimits <start2top@gmail.com>
2023-02-17 13:52:12 +0500

    added UX styles for intputs and contentitable values and styles for disabled inputblock

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,17 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // const carPrice = document.querySelector('#price'),
-    //     initPayment = document.querySelector('#initial-payment'),
-    //     leaseTerm = document.querySelector('#lease-term"'),
-    //     initPersentage = document.querySelector('#init_percentage'),
-    //     leasingSum = document.querySelector('#leasing_sum'),
-    //     monthlyPayment = document.querySelector('#monthly_payment');
+     const carPrice = document.querySelector('#price'),
+     initPayment = document.querySelector('#initial-payment'),
+     leaseTerm = document.querySelector('#lease-term'),
+     initPersentage = document.querySelector('#init_percentage'),
+     leasingSum = document.querySelector('#leasing_sum'),
+     monthlyPayment = document.querySelector('#monthly_payment'),
+     formContainer = document.querySelectorAll('.main_form_container'),
+     priceValue = document.querySelector('#price_value');
 
+         formContainer.forEach((e) => {
+              if(e.getAttribute('disabled') == 'true') {
+                e.style.opacity = '.3';
+                e.childNodes.forEach((elem) => {
+                       elem.setAttribute('contenteditable', 'false');
+                       elem.setAttribute('disabled', 'true');
+                    })
+              }
+        })
 
+    
 
+            
+    });
 
-
-
-
-
-
-});
+//     e.childNodes.forEach((elem) => {
+//         elem.setAttribute('disabled', '');
+//    })
