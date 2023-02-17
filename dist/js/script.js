6334179cd56985189a857ee6fe7f98e604f9de78
vlad_nolimits <start2top@gmail.com>
2023-02-17 14:16:27 +0500

    modified script for input forms using child elements of form instead of query selector

--- dist/js/script.js
+++ dist/js/script.js
@@ -5,24 +5,21 @@ document.addEventListener('DOMContentLoaded', () => {
      initPersentage = document.querySelector('#init_percentage'),
      leasingSum = document.querySelector('#leasing_sum'),
      monthlyPayment = document.querySelector('#monthly_payment'),
-     formContainer = document.querySelectorAll('.main_form_container'),
+     form = document.querySelector('.main_form'),
      priceValue = document.querySelector('#price_value');
 
-         formContainer.forEach((e) => {
-              if(e.getAttribute('disabled') == 'true') {
-                e.style.opacity = '.3';
-                e.childNodes.forEach((elem) => {
-                       elem.setAttribute('contenteditable', 'false');
-                       elem.setAttribute('disabled', 'true');
-                    })
-              }
-        })
-
+     // выбираем блоки инпутов и отключаем все при атрибьюте disabled
+        
+    form.childNodes.forEach((element) => {
+        if (element.getAttribute('disabled') == 'true' || element.disabled || element.getAttribute('disabled') == 'disabled') {
+                element.style.opacity = '.3';
+                element.childNodes.forEach((item) => {
+                    item.setAttribute('disabled', 'true');
+                    item.setAttribute('contenteditable', 'false');
+                })
+                }
+            })
     
 
             
     });
-
-//     e.childNodes.forEach((elem) => {
-//         elem.setAttribute('disabled', '');
-//    })
