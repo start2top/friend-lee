26264519f407ad48749dfd18689cce9604d2fea4
vlad_nolimits <start2top@gmail.com>
2023-02-20 12:52:38 +0500

    fixed errors on validator in html mode

--- dist/js/script.js
+++ dist/js/script.js
@@ -10,16 +10,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
      // выбираем блоки инпутов и отключаем все при атрибьюте disabled
         
-    form.childNodes.forEach((element) => {
-        if (element.getAttribute('disabled') == 'true' || element.disabled || element.getAttribute('disabled') == 'disabled') {
-                element.style.opacity = '.3';
-                element.childNodes.forEach((item) => {
-                    item.setAttribute('disabled', 'true');
-                    item.setAttribute('contenteditable', 'false');
+        form.childNodes.forEach((element) => {
+            if (element.getAttribute('disabled') == 'true' || element.disabled || element.getAttribute('disabled') == 'disabled') {
+                    element.style.opacity = '.3';
+                    element.childNodes.forEach((item) => {
+                        item.setAttribute('disabled', 'true');
+                        item.setAttribute('contenteditable', 'false');
+                    })
+                    }
                 })
-                }
-            })
     
-
+    
+        
             
     });
